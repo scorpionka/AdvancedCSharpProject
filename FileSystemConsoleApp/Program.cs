05e00b76b3d62c3016519d61cc45c70286c3b1ff
Aliaksei Kuzmitski <Aliaksei_Kuzmitski@epam.com>
2021-12-21 23:39:25 +0300

    Add project files.

---
+++ FileSystemConsoleApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FileSystemConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ FileSystemLibrary/FileSystemVisitor.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace FileSystemLibrary
+{
+    public class FileSystemVisitor
+    {
+    }
+}
